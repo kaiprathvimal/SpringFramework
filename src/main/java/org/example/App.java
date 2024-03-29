775f55f5395375c5968e35479d883d7044f2dcf7
Vimal Kaiprath <vkaiprath@ibizsoftinc.com>
2024-03-29 12:43:35 +0530

    Scope

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+        Programmer obj = (Programmer) context.getBean("programmer");
+        obj.comp();
+        Programmer obj1 = (Programmer) context.getBean("programmer");
+        obj1.comp();
+        
     }
 }
---
+++ src/main/java/org/example/Programmer.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Programmer {
+    public void comp(){
+        System.out.println("Compile the program");
+    }
+
+}
